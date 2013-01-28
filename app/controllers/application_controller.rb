c81c4da272a6040b229dc228ce1edad2c2f54386
mitsuhirookuno <mitsuhiro.okuno@gmail.com>
2013-01-28 11:42:14 +0900

    ログイン機能の試作

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def login?
+    current_user.presence
+  end
+
+  helper_method :current_user, :login?, :respond_to_model
+
+  def authenticate
+    if request.fullpath.match(/^\/qrapi/)
+      redirect_to '/sessions' unless login?
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails3Tutorial::Application.routes.draw do
   get "welcome/index"
+  resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
