3e9bb37512da7b20f8bb61f1fd1db1d704918478
mitsuhirookuno <mitsuhiro.okuno@gmail.com>
2013-01-28 12:58:35 +0900

    index.htmlを除去

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails3Tutorial::Application.routes.draw do
-  get "welcome/index"
+  root :to => 'welcome#index'
+#  get "welcome/index"
   resources :sessions
 
   # The priority is based upon order of creation:
